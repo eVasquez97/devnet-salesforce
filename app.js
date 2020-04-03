6d96f83b24bbc343f80aecb36859f9a9448f0475
Eduardo Vasquez <lavairelavaire@gmail.com>
2020-04-03 13:22:37 -0600

    Added basic queries

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 require("dotenv/config");
 const app = express();
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ client.connect( () =>{
     console.log('Connected');
 });
 
-// Queries
+// Queries - Case
 app.get('/cases', (req, res)=>{
     client.query('SELECT * FROM salesforce.case', (err, data)=>{
       if (err) {
@@ -30,7 +30,19 @@ app.get('/cases', (req, res)=>{
         res.json(data.rows);
       }
     });
+});
+
+// Queries - Notification__c
+app.get('/notifications', (req, res)=>{
+  client.query('SELECT * FROM salesforce.notification__c', (err, data)=>{
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(data.rows);
+    }
   });
+});
 
 // Inicializar
 app.listen(PORT, () => {
