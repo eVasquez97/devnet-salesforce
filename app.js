afb13af61bdafddf9f1958e052f637b243a9702b
Eduardo Vasquez <lavairelavaire@gmail.com>
2020-04-04 17:21:26 -0600

    Backend ready

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 
 // Queries - Case
 app.get('/cases', (req, res)=>{
-    client.query('SELECT * FROM salesforce.case', (err, data)=>{
+    client.query('SELECT * FROM salesforce.case ORDER BY id DESC', (err, data)=>{
       if (err) {
         console.log(err);
         res.status(400).send(err);
@@ -52,7 +52,7 @@ app.get('/cases/:id', (req, res)=>{
 
 // Queries - Notification__c
 app.get('/notifications', (req, res)=>{
-  client.query('SELECT * FROM salesforce.notification__c', (err, data)=>{
+  client.query('SELECT * FROM salesforce.notification__c ORDER BY id DESC', (err, data)=>{
     if (err) {
       console.log(err);
       res.status(400).send(err);
@@ -71,6 +71,26 @@ app.get('/notifications/:id', (req, res)=>{
       res.json(data.rows[0]);
     }
   });
+});
+
+app.post('/notifications', (req, res) => {
+  client.query("INSERT INTO " +
+    "salesforce.notification__c(details__c, category__c, type__c, eventid__c, status__c, issue_name__c, cisco_dna_link__c, priority__c, domain__c)" +
+    "VALUES($1, $2, $3, $4, $5, $6, $7, $8, %9) RETURNING *",
+    
+    [req.body.details__c, req.body.category__c, req.body.type__c, req.body.eventid__c, req.body.status__c, req.body.issue_name__c, 
+    req.body.cisco_dna_link__c, req.body.priority__c, req.body.domain__c],
+
+    (err, data) => {
+      if(err){
+        console.log('Error:', err);
+        res.status(400).send(err);
+      }
+      else {
+        console.log('Success. Inserting into database.');
+        res.send(data.rows[0]);
+      }
+    });
 });
 
 // Inicializar
